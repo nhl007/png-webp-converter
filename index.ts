c2f06486a63ea7ac564dc6cf3a22997358721857
nhl007 <apar.asif.an@gmail.com>
2025-01-21 21:20:45 +0600

    complete webp converter

--- index.ts
+++ index.ts
@@ -1,24 +1,27 @@
 import { existsSync, mkdirSync, readdir } from "fs";
 import path from "path";
 import sharp from "sharp";
+import { argv } from "node:process";
 
 async function convertPngToWebp(
   quality = 80,
   inputPath: string,
   outputPath: string
 ) {
-  return sharp(inputPath)
-    .resize({
-      width: 1180,
-    })
-    .webp({ quality: quality }) // Maximum quality
-    .toFile(outputPath)
-    .then(() => {
-      console.log(`Converted: ${inputPath} -> ${outputPath}`);
-    })
-    .catch((err) => {
-      console.error(`Error converting ${inputPath}:`, err);
-    });
+  return (
+    sharp(inputPath)
+      // .resize({
+      //   width: 1180,
+      // })
+      .webp({ quality: quality })
+      .toFile(outputPath)
+      .then(() => {
+        console.log(`Converted: ${inputPath} -> ${outputPath}`);
+      })
+      .catch((err) => {
+        console.error(`Error converting ${inputPath}:`, err);
+      })
+  );
 }
 
 function getPngFilesFromFolder(folderPath: string): Promise<string[]> {
@@ -29,7 +32,9 @@ function getPngFilesFromFolder(folderPath: string): Promise<string[]> {
       }
 
       const pngFiles = files.filter(
-        (file) => path.extname(file).toLowerCase() === ".png"
+        (file) =>
+          path.extname(file).toLowerCase() === ".png" ||
+          path.extname(file).toLowerCase() === ".jpeg"
       );
       resolve(pngFiles);
     });
@@ -46,10 +51,13 @@ async function convertAllPngFiles(folderPath: string, outputFolder: string) {
 
     for await (const file of pngFiles) {
       const inputPath = path.join(folderPath, file);
-      const outputFileName = file.replace(".png", ".webp");
+      let outputFileName = "";
+
+      file.includes(".jpeg")
+        ? (outputFileName = file.replace(".jpeg", ".webp"))
+        : (outputFileName = file.replace(".png", ".webp"));
       const outputPath = path.join(outputFolder, outputFileName);
 
-      // Convert each PNG file to WebP
       await convertPngToWebp(100, inputPath, outputPath);
     }
 
@@ -59,8 +67,14 @@ async function convertAllPngFiles(folderPath: string, outputFolder: string) {
   }
 }
 
-// Usage Example
-const inputFolder = "./in";
-const outputFolder = "./out";
+if (argv.length < 3) {
+  console.error("Input Folder Missing!");
+  process.exit(1);
+}
+
+const inputFolder = argv[2];
+const outputFolder = argv[3] ? argv[3] : "./out";
+
+console.log("Input Folder: ", inputFolder, "\n Output Folder: ", outputFolder);
 
 convertAllPngFiles(inputFolder, outputFolder);
